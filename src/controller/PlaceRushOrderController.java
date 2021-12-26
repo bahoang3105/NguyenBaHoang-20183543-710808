859eb10933616424b0e111baf75881f78fe3581d
Hoang <bahoang3105@gmail.com>
2021-12-26 16:46:47 +0700

    buoi5

--- src/controller/PlaceRushOrderController.java
+++ src/controller/PlaceRushOrderController.java
@@ -1,12 +1,10 @@
 package controller;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.logging.Logger;
 
 /**
- * This class controls the flow of place rush order use case
+ * This class controls the flow of place rush order use case in our AIMS project
  * @author HoangNB - 20183543
  */
 public class PlaceRushOrderController {
@@ -16,17 +14,17 @@ public class PlaceRushOrderController {
     public PlaceRushOrderController(RushOrderInputValidator rushOrderInputValidator) {
         this.rushOrderInputValidator = rushOrderInputValidator;
     }
-    
-    /**
-     * Specify provinces where support rush order
-     */
-    public static List<String> PROVINCES_SUPPORT_RUSH_ODER = List.of("Ha Noi");
 
     /**
-     * Specify list id of media that support rush order
-     * Only media id = 132 for testing
+     * Specify provinces that support rush order
      */
-    public static List<Integer> MEDIA_IDS_SUPPORT_RUSH_ORDER = List.of(132);
+    public static List<String> PROVINCES_SUPPORT_RUSH_ODER = List.of("HÃ  Ná»™i");
+
+    /**
+     * Specify media id that support rush order
+     * Only media id = 38 for testing
+     */
+    public static List<Integer> MEDIA_IDS_SUPPORT_RUSH_ORDER = List.of(38);
 
     /**
      * Just for logging purpose
@@ -36,7 +34,7 @@ public class PlaceRushOrderController {
     public static final String RECEIVE_TIME_FORMATTER = "dd-MM-yyyy HH:mm";
 
     /**
-     * Method checks if user's location supports rush order or not
+     * Method checks user's location support rush order or not
      * @param location User's province
      */
     public boolean isLocationSupportRushOrder(String location) {
@@ -55,7 +53,7 @@ public class PlaceRushOrderController {
     }
 
     /**
-     * Method checks if user's info support rush order or not
+     * Method checks user's info support rush order or not
      * @param location User's province
      * @param mediaID Cart's media id
      */
@@ -68,13 +66,7 @@ public class PlaceRushOrderController {
      * @param time User's receive time
      */
     public boolean validateReceiveTime(String time) {
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(RECEIVE_TIME_FORMATTER);
-            LocalDateTime.parse(time, formatter);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+        return rushOrderInputValidator.isValidReceiveTime(time, RECEIVE_TIME_FORMATTER);
     }
 
     /**
@@ -82,7 +74,7 @@ public class PlaceRushOrderController {
      * @param info User's rush order info
      */
     public boolean validateRushOrderInfo(String info) {
-        return validateBasicString(info);
+        return rushOrderInputValidator.isValidRushOrderInfo(info);
     }
 
     /**
@@ -90,30 +82,8 @@ public class PlaceRushOrderController {
      * @param instruction User's rush order instruction
      */
     public boolean validateRushOrderInstruction(String instruction) {
-        return validateBasicString(instruction);
+        return rushOrderInputValidator.isValidRushOrderInstruction(instruction);
     }
 
-    @SuppressWarnings("deprecation")
-	private boolean validateBasicString(String info) {
-        if (info == null || info.isEmpty()) {
-            return false;
-        }
 
-        boolean isValid = true;
-        for (char ch : info.toCharArray()) {
-            if ( Character.isSpace(ch) ) {
-                continue;
-            }
-            if ( Character.isDigit(ch) ) {
-            	continue;
-            }
-            if ( Character.isLetter(ch) ) {
-            	continue;
-            }
-            isValid = false;
-            break;
-        }
-
-        return isValid;
-    }
 }
